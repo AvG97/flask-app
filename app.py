46d6350305b56e558131aa73708ba40f5b72adca
AvG97 <avishekganguly.26@gmail.com>
2021-08-24 09:34:22 +0530

    Change layout

--- app.py
+++ app.py
@@ -4,18 +4,24 @@ import json
 import plotly
 import plotly.express as px
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
-@app.route('/')
+@app.route("/")
+@app.route("/index")
+def index():
+    
+    return render_template("index.html")
+
+@app.route("/graph1")
 def ndash():
-	
+    
 	df = pd.DataFrame( { "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas" ], "Amount": [4,2,1,2,4,5], "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"] })
 	
 	fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 	
 	graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 	
-	return render_template("ndash.html", graphJSON=graphJSON)
+	return render_template("graph1.html", graphJSON=graphJSON)
 	
 if __name__ == "__main__":
 	app.run()
